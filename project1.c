b06b79c22dafe381a09a1f6583de4434ef3eb218
hbji0817 <hbji0817@gmail.com>
2025-03-10 21:07:10 +0900

    Add files via upload

---
+++ project1.c
@@ -0,0 +1,52 @@
+﻿#include <stdio.h>
+
+int main(void){
+	//프로젝트
+	//경찰관이 범죄자의 정보를 입수 (조서작성)
+	//이름? 나이? 몸무게? 키? 범죄명?
+	char name[20];
+	printf("이름이 뭐에요?");
+	scanf("%s",&name);
+	
+	//char = 문자형 변수 선언
+	
+	int age;
+	printf("몇살이에요?");
+	scanf("%d", &age);
+	
+	//int = 정수형 변수 선언
+	
+	float weight;
+	printf("몸무게는 몇 kg이에요?");
+	scanf("%f",&weight);
+	
+	//float = 실수형 변수 선언
+	
+	double height;
+	printf("키는 몇 센치미터에요?");
+	scanf("%lf", &height);
+	
+	//double = 실수형 변수 선언, 소수점이하 5자리까지 가능 %lf = 1자리, %.1lf = 소수점 1자리
+	
+	char what[20];
+	printf("무슨 일을 하세요?");
+	scanf("%s",what);
+	
+	
+	
+	//내용 출력
+	
+	printf("\n\n———  정보  ———\n\n");
+	printf("이름  : %s \n", name);
+	printf("나이  : %d \n", age);
+	printf("몸무게: %.2f \n",weight);
+	printf("키    : %.2lf \n", height);
+	printf("직업  : %s \n", what);
+	
+	
+	
+	
+	
+	return 0;
+	
+}
---
+++ scanf.c
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+
+//scanf = 키보드 입력을 받아서 저장
+
+int main(void)
+{
+	/*int input;
+	printf("값을 입력하세요");
+	scanf(("%d", &input);
+	printf("입력값 : %d\n", input);
+	
+	return 0;
+	*/
+	
+	/*int one, two, three;
+	printf("3개의 정수를 입력하세요 : ");
+	scanf("%d, %d, %d", &one, &two, &three);
+	printf("첫번째 값 : %d\n", one);
+	printf("두번째 값 : %d\n",two);
+	printf("세번째 값 : %d\n",three);
+	return 0;
+	*/
+	
+	// 문자(한 글자) , 문자열(한글자 이상의 여러 글자)
+	/*char c = 'A';
+	printf("%c\n", c); */
+	
+	char str[256];
+	scanf("%s", str, sizeof(str));
+	printf("%s\n",str);
+	
+	
+	
+	
+	return 0;
+}
