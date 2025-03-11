a5898ccc16e7d0ec95b7e36eea8702ef7450361b
hbji0817 <hbji0817@gmail.com>
2025-03-11 14:26:16 +0900

    Add files via upload

---
+++ project4.c
@@ -0,0 +1,22 @@
+﻿//if else 문
+
+#include <stdio.h>
+
+int main(void)
+{
+	//버스를 탄다고 할 때 학생 성인 구분/ 성인= 20이상, 어린이 구분/12이하
+	int age;
+	printf("몇살이신가요?\n");
+	scanf("%d\n", &age);
+	
+	
+	if (age >= 20)
+		printf("성인입니다");
+	
+	else if(age > 12 && age <= 20)
+		printf("학생입니다");	
+	else if(age <= 12 && age >0)
+		printf("어린이입니다");
+	
+	return 0;
+}
---
+++ project5.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+int main(void){
+	
+	//break / continue
+	//1번부터 30번까지 있는 반에서 1번에서 5번까지 조별 발표
+	
+	int a;
+	
+	printf("몇 번까지 발표하나요\n");
+	scanf("%d\n", &a);
+	
+	for (int i = 1 ; i <= 30;i++){
+		if (i >a){
+			printf("나머지는 집으로");
+			break;
+		}
+		printf ("%d번 학생은 발표 준비하세요\n", i);
+		
+	}
+	
+	
+	return 0;
+	
+}
