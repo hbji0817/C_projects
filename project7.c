88a9619e5d5d08b28e32eb73deaac55f7eda1b13
hbji0817 <hbji0817@gmail.com>
2025-03-17 13:26:08 +0900

    Add files via upload

---
+++ project7.c
@@ -0,0 +1,57 @@
+﻿
+
+
+#include <stdio.h>
+
+
+
+//선언
+void p(int num);
+
+
+int main(void)
+{
+	//function
+	//계산기
+	
+	int num = 2;
+	p(num);
+	
+	num = num + 3;
+	p(num);
+	
+	num -= 1;
+	p(num);
+	
+	
+
+	
+	return 0;
+	
+}
+
+
+//정의
+void p(int num)
+{
+	printf("num 은 %d 입니다\n",num);
+}
+
+/* 
+void라는 포인터에 대해 배움
+void는 자료형이 정해지지 않은 범용 포인터
+p는 포인터 변수임!
+그저 많이 쓰이는 것 뿐 원하는 아무거나 변수로 지정해도 됨
+
+()안에는 printf 안에 %d에서 쓰일 변수를 적으면 됨
+
+그리고 무조건 앞에 선언을 하고 
+아예 나가서 맨 뒤에 얘가 뭘 의미하는지 정의를 적어야함
+
+*/
+
+
+
+ 
+ 
+ 
---
+++ project8.c
@@ -0,0 +1,57 @@
+﻿#include <stdio.h>
+
+void p(int num);
+
+//void function_without_return();
+//int function_with_return(void);
+//void function_without_params();
+void function_with_params(int a, int b, int c);
+
+//int apple(int total, int ate);
+
+int main(void){
+	
+	//반환값이 없는 함수
+//	function_without_return();
+	
+	
+	//반환값이 있는 함수
+//	int ret = function_with_return();
+//	p(ret);
+	
+	//전달값이 없는 함수
+//	function_without_params();
+	
+	//전달값이 있는 함수
+	function_with_params(1, 2, 3);
+	
+	
+	return 0;
+
+}
+
+//void function_without_return()
+//{
+//	printf("반환값이 없는 함수입니다\n");
+//}
+
+//int function_with_return(void)
+//{
+//	printf("반환값이 있는 함수입니다\n");
+//	return 10;	            
+//}
+
+//void function_without_params()
+//{
+//	printf("전달값이 없는 함수입니다");
+//}
+
+void function_with_params(int a, int b, int c)
+{
+	printf("전달값이 있는 함수이며, 전달받은 값은 %d, %d, %d입니다 \n", a, b, c);
+}
+
+void p(int num)
+{
+	printf("출력값: %d\n", num);
+}
