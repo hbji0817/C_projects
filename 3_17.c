bc74332fff24fcfd008adc123c44c4abd5e216be
hbji0817 <hbji0817@gmail.com>
2025-03-17 13:56:11 +0900

    Add files via upload

---
+++ 3_17.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+  int number;
+  number = 1234;
+
+  int computer = 3;
+
+  double pi;
+  pi = 3.1415926;
+
+  printf("Hello world");
+  printf("%d\n", number);
+  printf("I have %d computer\n", computer);
+  printf("pi = %f\n", pi);
+
+  return 0;
+}
