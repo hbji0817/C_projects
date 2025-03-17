51d3b95f97a29952f3446ff40dfcbd9ceb9aabed
hbji0817 <hbji0817@gmail.com>
2025-03-17 14:07:07 +0900

    Update 3_17.c

--- 3_17.c
+++ 3_17.c
@@ -9,10 +9,15 @@ int main(void) {
   double pi;
   pi = 3.1415926;
 
+  char letter;
+  letter = 'a';
+
   printf("Hello world");
   printf("%d\n", number);
   printf("I have %d computer\n", computer);
   printf("pi = %f\n", pi);
+  printf("Letter I selected : %c\n", letter);
+  
 
   return 0;
 }
