aa0340fe1538fd5f9200733290b517c35d75b3c0
hbji0817 <hbji0817@gmail.com>
2025-03-19 12:40:51 +0900

    Add files via upload

---
+++ 3_19.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main(void) {
+  // /* comment 주석
+  //   10 + 20 x 3 계산 */
+  
+  // int number;
+  // int number2;
+  // double number3;
+  int number4;
+
+  // /* arithmatic operation */
+  // number = 10 + 20 * 3;
+
+  // printf("%d\n", number);
+
+
+  
+  // /* using parenthesis */
+  // number2 = (10 + 20) * 3;
+
+  // printf("%d\n", number2);
+
+  // /*division */
+  // //number3 = (double)10 / 3; 괄호에 변환하고자 하는 함숫값을 넣으면 됨
+  // number3 = 10 / 3;
+  // // 정수형 문자열과 정수형 문자열의 계산에서의 결과는 정수형으로 저장됨
+  // //따라서 double(float)에 실수형 문자열로 저장하려면 문자열을 double(float)로 변환해야함
+  // //(실수형으로)(둘 중 하나만 바꿔도 됨)
+
+  /* another division example */
+
+  number4 = (10 + 2) / 3;
+  printf("%d\n", number4);
+  
+
+  // printf("%.3f\n", number3);
+
+
+  int count;
+
+  
+  count = 1;
+  printf("%d\n", count);
+
+  count = count + 1;
+
+  
+  
+  
+  return 0;
+}
+
+
+
+
