e9762f5e3c30228f66bc80a0613c0589b5610405
hbji0817 <hbji0817@gmail.com>
2025-04-18 20:12:05 +0900

    Add files via upload

---
+++ 6exercise2.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h> //표준 입출력 라이브러리 사용
+
+int main() //메인함수 선언
+{
+	int a, b; //처음 숫자 a, 나중 숫자 b 선언
+	scanf("%d %d",&a, &b); //a,b 입력 받기
+	
+	for(int i = a ; i <= b ; i++) //앞자리 숫자를 a 부터 b까지 반복하는 for문
+	{
+		for(int j = a ; j<= b ; j++) //뒷자리 숫자를 a 부터 b까지 반복하는 for문
+		{
+			printf("%d * %d = %d\n",i,j,i*j); //앞자리 i와 뒷자리 j 그리고 둘을 곱한 i*j 출력
+		}
+		printf("\n"); //각 단이 끝날때마다 문단 바꾸기
+	}
+	return 0; //함수 종료
+}
---
+++ 6exercise3.c
@@ -0,0 +1,37 @@
+﻿#include <stdio.h> //표준 입출력 라이브러리 사용
+
+int main() //메인함수 선언
+{
+	int n, m; //피라미드 높이 n, 쓰이는 숫자의 최댓값 m 선언
+	int num=0; //입력되는 숫자 num 선언
+	
+	
+	scanf("%d %d",&n,&m); //n, m 입력 받기
+	
+	for(int i = 1 ; i <= n ; i++)  //피라미드 높이 만큼 반복
+	{
+		
+		for(int j = 1 ; j <= n - i; j++) //띄어쓰기 n-1번 출력
+		{
+			printf(" "); //띄어쓰기기출력
+		}
+		for(int k = 1 ; k <= i*2-1; k++) //숫자 num출력 ,2i-1개
+		{
+			if(num != m+1) // m까지출력하고 
+			{
+				printf("%d",num); //num값 출력
+				num += 1; //num값 하나 올리기
+			}
+			else //넘으면 다시 초기화
+			{
+				printf("0"); //넘었으니 다음은 0 출력
+				num = 1; //0출력했으니 num을 1로 설정
+			}
+				
+		}
+		printf("\n"); //줄 바꾸기
+		
+	}
+	return 0; //함수 종료
+}
+
